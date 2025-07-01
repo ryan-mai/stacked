02c320edccf975057ba53bd62355b786fe79117f
Your NameRyan <inoober2008@gmail.com>
2025-07-01 19:04:44 -0400

    Set up GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/stacked/', // Replace 'stacked' with your actual repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
